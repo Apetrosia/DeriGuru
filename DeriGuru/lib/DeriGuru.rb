e748dd5d9a76f99f3d40e867bf9777d1d493079f
Анна Петросян <apetrosia@sfedu.ru>
2023-05-17 15:22:57 +0300

    Add gem project

---
+++ DeriGuru/lib/DeriGuru.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "DeriGuru/version"
+
+module DeriGuru
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ DeriGuru/lib/DeriGuru/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module DeriGuru
+  VERSION = "0.1.0"
+end
