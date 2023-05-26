a3726c2ecbf2b318b3649b9100e971dd4c522748
Lisa <92753486+Luizett@users.noreply.github.com>
2023-05-26 21:10:37 +0300

    Validation (#1)
    * Add validation function
    
    * Add no variable tests for validation function
    
    * Add tests for validation function

--- DeriGuru/test/test_DeriGuru.rb
+++ DeriGuru/test/test_DeriGuru.rb
@@ -1,14 +1,10 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require_relative "test_helper"
 
 # Basic gem tests
 class TestDeriGuru < Minitest::Test
   def test_has_a_version_number
     refute_nil ::DeriGuru::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
   end
 end
