8a486330ca72dee86708920c591c5171ad50e77b
Lisa <92753486+Luizett@users.noreply.github.com>
2023-05-26 21:04:28 +0300

    Add tests for validation function

--- DeriGuru/test/test_validation.rb
+++ DeriGuru/test/test_validation.rb
@@ -5,96 +5,184 @@ require_relative "../lib/DeriGuru/validation"
 
 class TestDeriCalculation < Minitest::Test
   include DeriGuru
+
   # Tests without exceptions
-  # Tests without exceptions for poly with no variable
-  def test_no_var_int_digit
+  def test_no_var_digit
     assert_silent { validation_check("1") }
-  end
-  def test_no_var_neg_int_digit
     assert_silent { validation_check("-1") }
-  end
-  def test_no_var_double_digit
     assert_silent { validation_check("2.3") }
-  end
-  def test_no_var_neg_double_digit
     assert_silent { validation_check("-20.3") }
   end
-  # tests integers operations
-  def test_no_var_int_with_plus
+
+  def test_no_var_digits_operations
     assert_silent { validation_check("5+46") }
-  end
-  def test_no_var_int_with_minus
     assert_silent { validation_check("22-4") }
-  end
-  def test_no_var_int_with_div
-    assert_silent { validation_check("9/612") }
-  end
-  def test_no_var_int_with_prod
-    assert_silent { validation_check("8*8") }
-  end
-  # test double with operations
-  def test_no_var_double_with_plus
     assert_silent { validation_check("5.8+6.9") }
-  end
-  def test_no_var_double_with_minus
     assert_silent { validation_check("22.2-4.4") }
-  end
-  def test_no_var_double_with_div
-    assert_silent { validation_check("9.8/644.444") }
-  end
-  def test_no_var_double_with_prod
-    assert_silent { validation_check("8.811*8.8") }
-  end
-  # Test with negative digits
-  def test_no_var_neg_case1
     assert_silent { validation_check("-1.6-598") }
-  end
-  def test_no_var_neg_case2
     assert_silent { validation_check("-1001+9") }
-  end
-  def test_no_var_neg_case3
-    assert_silent { validation_check("-6.9/5") }
-  end
-  def test_no_var_neg_case4
-    assert_silent { validation_check("-6/-52.7") }
-  end
-  def test_no_var_neg_case5
-    assert_silent { validation_check("-6.6*5") }
-  end
-  def test_no_var_neg_case6
-    assert_silent { validation_check("-6*-5.0") }
-  end
-  def test_no_var_mixed_expression1
     assert_silent { validation_check("3.3+6") }
   end
-  def test_no_var_mixed_expression2
-    assert_silent { validation_check("3+6.8-2*7+8/4") }
-  end
-  def test_no_var_mixed_expression3
-    assert_silent { validation_check("3+6.9*9.12*0") }
-  end
-  def test_no_var_mixed_expression4
-    assert_silent { validation_check("93.21/0-12-12/0.123") }
-  end
-  def test_no_var_mixed_expression5
-    assert_silent { validation_check("0.123/0.345/0.278") }
-  end
-  def test_no_var_mixed_expression6
-    assert_silent { validation_check("0.123*-0.345*0.278") }
-  end
 
-  # Tests without exceptions for poly with one variable
-  def test_one_variable_simple
+  def test_one_variable
     assert_silent { validation_check("x") }
+    assert_silent { validation_check("-x") }
   end
-  #  Tests without exceptions for poly with one variable
-  #  Tests without exceptions for poly with two variable
-  #  Tests without exceptions for poly with three variable
 
-  #  Tests about unexpected symbols
+  def test_one_variable_operation
+    assert_silent { validation_check("x+x") }
+    assert_silent { validation_check("x-x") }
+  end
 
-  #  Tests about wrong symbol seq
+  def test_one_var_with_coeffitient
+    assert_silent { validation_check("2*x") }
+    assert_silent { validation_check("-2*x") }
+    assert_silent { validation_check("2.2*x") }
+    assert_silent { validation_check("-2.9*x") }
+  end
 
-  #  Tests about
+  def test_one_var_with_coef_oper
+    assert_silent { validation_check("4*x+5*x") }
+    assert_silent { validation_check("4*x-5*x") }
+    assert_silent { validation_check("-4.98*x-5.57*x") }
+    assert_silent { validation_check("4.74*x+57.98*x") }
+  end
 
+  def test_one_var_with_degree
+    assert_silent { validation_check("x^0") }
+    assert_silent { validation_check("x^1") }
+    assert_silent { validation_check("x^2") }
+    assert_silent { validation_check("x^3") }
+  end
+
+  def test_one_var_with_degree_oper
+    assert_silent { validation_check("x^2+x^3") }
+    assert_silent { validation_check("x^2-x^3") }
+    assert_silent { validation_check("x^2+x^3+x^6") }
+    assert_silent { validation_check("-x^2-x^3-x^6") }
+    assert_silent { validation_check("-x^2+x^3-x^6") }
+    assert_silent { validation_check("x^2-x^3+x^6") }
+  end
+
+  def test_one_var_deg_coefs_oper
+    assert_silent { validation_check("12*x^2+0*x^3") }
+    assert_silent { validation_check("-1*x^2-244*x^3") }
+    assert_silent { validation_check("2*x^2+48*x^3+712*x^1") }
+    assert_silent { validation_check("12*x^2-164*x^3-9*x^6") }
+    assert_silent { validation_check("-12*x^1+164*x^3-9*x^5") }
+    assert_silent { validation_check("12*x^2-164*x^0+9*x^6") }
+    assert_silent { validation_check("12.678*x^10+0.001*x^3") }
+    assert_silent { validation_check("-1.2*x^2-244.44*x^16") }
+    assert_silent { validation_check("2.2*x^2+48.34*x^3+712.0*x^1") }
+    assert_silent { validation_check("-12.89*x^2-164.18*x^3-9.2*x^4") }
+    assert_silent { validation_check("-12.87*x^2+164.0*x^3-9*x^6") }
+    assert_silent { validation_check("12.08*x^2-164.0001*x^3+913.9*x^6") }
+  end
+
+  def test_two_var_opers
+    assert_silent { validation_check("x+y") }
+    assert_silent { validation_check("x-y") }
+    assert_silent { validation_check("y-x") }
+    assert_silent { validation_check("-x-y") }
+  end
+
+  def test_two_var_opers_with_coefs
+    assert_silent { validation_check("5*x+12*y") }
+    assert_silent { validation_check("5*x-3*y") }
+    assert_silent { validation_check("0*y-x") }
+    assert_silent { validation_check("-x-9*y") }
+    assert_silent { validation_check("0.12*x+87.9*y") }
+    assert_silent { validation_check("13.0*x-98*y") }
+    assert_silent { validation_check("1.1*y-x") }
+    assert_silent { validation_check("-195.1*x-0.08y") }
+  end
+
+  def test_two_var_opers_with_degree
+    assert_silent { validation_check("x^1+y^3+x^2") }
+    assert_silent { validation_check("x^7+y^32+y^2") }
+    assert_silent { validation_check("x^7+y^32+y^2+x^4") }
+    assert_silent { validation_check("x^1-y^3+x^2") }
+    assert_silent { validation_check("x^7-y^32-y^2") }
+    assert_silent { validation_check("x^7-y^32+y^2-x^4") }
+  end
+
+  def test_two_var_deg_and_coefs
+    assert_silent { validation_check("y^32+3*y^2+9.8*x") }
+    assert_silent { validation_check("98*x^7+1.17*y^32+9*y^2-x^4") }
+    assert_silent { validation_check("9*x^1-1*y^3+8*x^2") }
+    assert_silent { validation_check("83.0*x^7-12.9*y^32-89.944*y^2") }
+    assert_silent { validation_check("12.8*x^7-12.98*y^32+0*y^2-12.45*x^4") }
+  end
+
+  def test_three_var_opers
+    assert_silent { validation_check("x+y+z") }
+    assert_silent { validation_check("x-y-z") }
+  end
+
+  def test_three_var_with_coefs
+    assert_silent { validation_check("2*x+75*y+123.74*z") }
+    assert_silent { validation_check("2*x-75*y-123.74*z") }
+  end
+
+  def test_three_var_with_degree
+    assert_silent { validation_check("x^3+y^1+z^2") }
+    assert_silent { validation_check("-x^3-y^1-z^2") }
+  end
+
+  def test_three_var_deg_and_coefs
+    assert_silent { validation_check("78.89*x^3+0*y^1+12*z^2") }
+    assert_silent { validation_check("x^3+x^0+y^1+y^2+z^2+z^3") }
+    assert_silent { validation_check("-12*x^3-14.98*x^2+x+1*y^1+14.78*y^4+34*y^2-56.001*z^2-z+z^3") }
+  end
+
+  # Исключения: - неоджиданный(запрещённый) символ
+
+  def test_unexpected_symb_division
+    assert_raises(StandardError) { validation_check("x/x") }
+    assert_raises(StandardError) { validation_check("12.95/34") }
+  end
+
+  def test_unexpected_symb_space
+    assert_silent { validation_check("-x^3 -y^1-z^2") }
+  end
+
+  def test_unexpected_symb_bracket
+    assert_raises(StandardError) { validation_check("x^(-12-9)+x^(12-4)") }
+    assert_raises(StandardError) { validation_check("x+(x-y)") }
+  end
+
+  def test_unexpected_symb_rand
+    assert_raises(StandardError) { validation_check("x^7+x^0!") }
+    assert_raises(StandardError) { validation_check("1<5+x>y") }
+  end
+
+  #  Exception - неправильно употреблён символ
+
+  def test_wrong_placed_symbol_prod
+    assert_raises(StandardError) { validation_check("x^2*x^3*x^6") }
+    assert_raises(StandardError) { validation_check("x*x") }
+    assert_raises(StandardError) { validation_check("6*6") }
+    assert_raises(StandardError) { validation_check("6*7*x^2+x") }
+  end
+
+  def test_wrong_placed_symb_duplicts
+    assert_raises(StandardError) { validation_check("x++x") }
+    assert_raises(StandardError) { validation_check("x--x") }
+    assert_raises(StandardError) { validation_check("4.2**x") }
+    assert_raises(StandardError) { validation_check("4..02*x") }
+    assert_raises(StandardError) { validation_check("x^^3") }
+  end
+
+  def test_wrong_placed_symb_letters
+    assert_raises(StandardError) { validation_check("2*x+xy") }
+    assert_raises(StandardError) { validation_check("hello") }
+  end
+
+  def test_wrong_placed_symbol
+    assert_raises(StandardError) { validation_check("x+-x") }
+    assert_raises(StandardError) { validation_check("x-+x") }
+    assert_raises(StandardError) { validation_check("x^-4+x") }
+    assert_raises(StandardError) { validation_check("4x^3") }
+    assert_raises(StandardError) { validation_check("x9-3+y") }
+  end
 end
