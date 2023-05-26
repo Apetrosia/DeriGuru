91897691bae80826c75cb917dfab856013a7b377
latinrice <92548405+latinrice@users.noreply.github.com>
2023-05-26 19:21:53 +0300

    add errors for validation

--- DeriGuru/lib/exceptions.rb
+++ DeriGuru/lib/exceptions.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
 module DeriGuru
-  # Basic DeriGuru exception class
-  class DeriGuruError < StandardError
-    def to_s
-      'Failed to differentiate a polynomial'
-    end
-  end
+  # Basic DeriGuru error class
+  class DeriGuruError < StandardError; end
 
-  #TODO: implement specific exceptions
+  # Error during validation
+  class PolyValidationError < DeriGuruError; end
+
+  # Symbol met wasn't a digit or operation
+  class UnexpectedSymbolError < PolyValidationError; end
+
+  # Acceptable symbol is used incorrectly
+  class IncorrectSymbolError < PolyValidationError; end
 end
