a4858f3e6156298e339450b581afb703a0d83ce2
Анна Петросян <apetrosia@sfedu.ru>
2023-05-28 13:23:00 +0300

    Fix tests

--- DeriGuru/test/test_deri_parser.rb
+++ DeriGuru/test/test_deri_parser.rb
@@ -8,50 +8,50 @@ class TestDeriParser < Minitest::Test
   include DeriGuru
 
   def setup
-    @poly_parser = PolyParser.new
+    @poly_differ = PolyDiffer.new
   end
 
-  def test_poly_parser_has_needed_method
-    assert_respond_to @poly_parser, :poly_parse, is_private: true
+  def test_poly_differ_has_needed_method
+    assert_respond_to @poly_differ, :poly_differ, is_private: true
   end
 
   def test_empty_poly
-    assert_empty(@poly_parser.send(:poly_parse, ""))
+    assert_empty(@poly_differ.send(:poly_differ, ""))
   end
 
   def test_one_int_var
-    assert_equal([[1, 1]], @poly_parser.send(:poly_parse, "x"))
-    assert_equal([[0, 1]], @poly_parser.send(:poly_parse, "1"))
-    assert_equal([[1, 2]], @poly_parser.send(:poly_parse, "2x"))
-    assert_equal([[3, 1]], @poly_parser.send(:poly_parse, "x^3"))
-    assert_equal([[4, 5]], @poly_parser.send(:poly_parse, "5x^4"))
-    assert_equal([[1, -1]], @poly_parser.send(:poly_parse, "-x"))
-    assert_equal([[0, -1]], @poly_parser.send(:poly_parse, "-1"))
-    assert_equal([[1, -2]], @poly_parser.send(:poly_parse, "-2x"))
-    assert_equal([[3, -1]], @poly_parser.send(:poly_parse, "-x^3"))
-    assert_equal([[4, -5]], @poly_parser.send(:poly_parse, "-5x^4"))
+    assert_equal([[1, 1, '']], @poly_differ.send(:poly_differ, "x", 'x'))
+    assert_equal([[0, 1, '']], @poly_differ.send(:poly_differ, "1", 'x'))
+    assert_equal([[1, 2, '']], @poly_differ.send(:poly_differ, "2x", 'x'))
+    assert_equal([[3, 1, '']], @poly_differ.send(:poly_differ, "x^3", 'x'))
+    assert_equal([[4, 5, '']], @poly_differ.send(:poly_differ, "5x^4", 'x'))
+    assert_equal([[1, -1, '']], @poly_differ.send(:poly_differ, "-x", 'x'))
+    assert_equal([[0, -1, '']], @poly_differ.send(:poly_differ, "-1", 'x'))
+    assert_equal([[1, -2, '']], @poly_differ.send(:poly_differ, "-2x", 'x'))
+    assert_equal([[3, -1, '']], @poly_differ.send(:poly_differ, "-x^3", 'x'))
+    assert_equal([[4, -5, '']], @poly_differ.send(:poly_differ, "-5x^4", 'x'))
   end
 
   def test_one_float_var
-    assert_equal([[1, 5.8]], @poly_parser.send(:poly_parse, "5.8x"))
-    assert_equal([[2, -5.8]], @poly_parser.send(:poly_parse, "-5.8x^2"))
-    assert_equal([[0, 2.4]], @poly_parser.send(:poly_parse, "2.4"))
-    assert_equal([[0, -2.4]], @poly_parser.send(:poly_parse, "-2.4"))
+    assert_equal([[1, 5.8, '']], @poly_differ.send(:poly_differ, "5.8x", 'x'))
+    assert_equal([[2, -5.8, '']], @poly_differ.send(:poly_differ, "-5.8x^2", 'x'))
+    assert_equal([[0, 2.4, '']], @poly_differ.send(:poly_differ, "2.4", 'x'))
+    assert_equal([[0, -2.4, '']], @poly_differ.send(:poly_differ, "-2.4", 'x'))
   end
 
   def test_same_power_vars
-    assert_equal([[3, 5]], @poly_parser.send(:poly_parse, "2x^3+3x^3"))
-    assert_equal([[1, 5]], @poly_parser.send(:poly_parse, "x+4x"))
-    assert_equal([[2, 2.3]], @poly_parser.send(:poly_parse, "3.4x^2-1.1x^2"))
-    assert_equal([[1, 2.4]], @poly_parser.send(:poly_parse, "6x-3.6x"))
-    assert_equal([[2, 9.5]], @poly_parser.send(:poly_parse, "5x^2+4.5x^2"))
-    assert_equal([[0, -8]], @poly_parser.send(:poly_parse, "10-18"))
+    assert_equal([[3, 5, '']], @poly_differ.send(:poly_differ, "2x^3+3x^3", 'x'))
+    assert_equal([[1, 5, '']], @poly_differ.send(:poly_differ, "x+4x", 'x'))
+    assert_equal([[2, 2.3, '']], @poly_differ.send(:poly_differ, "3.4x^2-1.1x^2", 'x'))
+    assert_equal([[1, 2.4, '']], @poly_differ.send(:poly_differ, "6x-3.6x", 'x'))
+    assert_equal([[2, 9.5, '']], @poly_differ.send(:poly_differ, "5x^2+4.5x^2", 'x'))
+    assert_equal([[0, -8, '']], @poly_differ.send(:poly_differ, "10-18", 'x'))
   end
 
   def test_random_power_vars
-    assert_equal([[1, 5], [2, 7]], @poly_parser.send(:poly_parse, "x+4x+7x^2"))
-    assert_equal([[1, 4.5], [2, 8]], @poly_parser.send(:poly_parse, "x^2+4.5x+7x^2"))
-    assert_equal([[0, -1], [1, 2]], @poly_parser.send(:poly_parse, "5-6+2x"))
-    assert_equal([[0, -5], [7]], @poly_parser.send(:poly_parse, "2x+4+5x-9"))
+    assert_equal([[1, 5, ''], [2, 7, '']], @poly_differ.send(:poly_differ, "x+4x+7x^2", 'x'))
+    assert_equal([[1, 4.5, ''], [2, 8, '']], @poly_differ.send(:poly_differ, "x^2+4.5x+7x^2", 'x'))
+    assert_equal([[0, -1, ''], [1, 2, '']], @poly_differ.send(:poly_differ, "5-6+2x", 'x'))
+    assert_equal([[0, -5, ''], [1, 7, '']], @poly_differ.send(:poly_differ, "2x+4+5x-9", 'x'))
   end
 end
